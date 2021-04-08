3ccd03f468c968b0d426904f4f8978f145cb525b
Jonah Meijers <jonahmeijers97@gmail.com>
2021-04-08 18:42:58 +0200

    update web setup

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,12 +5,11 @@ export default defineConfig({
   publicDir: 'public',
   build: {
     outDir: 'dist',
-  },
-  esbuild: {
     minify: true,
-    sourcemap: 'external',
-    format: 'esm',
-    target: 'es2017',
-    treeShaking: true,
+    cssCodeSplit: true,
+    target: 'es2015',
+    rollupOptions: {
+      treeshake: true,
+    },
   },
 })
